370100d5c3d6f5074e166b797659641136dc0a4b
toitouine <antoine.mechulam@gmail.com>
2024-03-04 20:02:27 +0100

    Adding black pawn move generation

--- src/game/board/move/MoveGenerator.java
+++ src/game/board/move/MoveGenerator.java
@@ -108,6 +108,55 @@ public class MoveGenerator {
   }
 
   private void addBlackPawnMoves(ArrayList<Move> moves) {
+    long pions = board.pieceBitboard[Piece.Pion + Piece.NumberOfType];
+    long empty = ~board.colorBitboard[0] & ~board.colorBitboard[1];
+
+    // Avance simple des pions (et promotion)
+    long onepush = (pions << 8) & empty;
+    while (onepush != 0) {
+      int endSquare = Long.numberOfTrailingZeros(onepush);
+      if (endSquare >= 56) addPromotionMoves(moves, endSquare-8, endSquare);
+      else moves.add(new Move(endSquare - 8, endSquare));
+      onepush &= onepush - 1;
+    }
+
+    // Avance double des pions
+    long doublepush = (((pions & Bitboard.rank7) << 8) & empty) << 8 & empty;
+    while (doublepush != 0) {
+      int endSquare = Long.numberOfTrailingZeros(doublepush);
+      moves.add(new Move(endSquare - 16, endSquare, MoveFlag.DoubleAvance));
+      doublepush &= doublepush - 1;
+    }
+
+    // Capture droite (et promotion)
+    long captureRight = ((~Bitboard.Hfile & pions) << 9) & board.colorBitboard[Player.White];
+    while (captureRight != 0) {
+      int endSquare = Long.numberOfTrailingZeros(captureRight);
+      if (endSquare >= 56) addPromotionMoves(moves, endSquare - 9, endSquare);
+      else moves.add(new Move(endSquare - 9, endSquare));
+      captureRight &= captureRight - 1;
+    }
+
+    // Capture gauche (et promotion)
+    long captureLeft = ((~Bitboard.Afile & pions) << 7) & board.colorBitboard[Player.White];
+    while (captureLeft != 0) {
+      int endSquare = Long.numberOfTrailingZeros(captureLeft);
+      if (endSquare >= 56) addPromotionMoves(moves, endSquare - 7, endSquare);
+      else moves.add(new Move(endSquare - 7, endSquare));
+      captureLeft &= captureLeft - 1;
+    }
+
+    // En passant
+    if (board.enPassantSquare[Player.White] == null) return;
+
+    int caseEnPassant = board.enPassantSquare[Player.White];
+    long mangeurs = pions & ( (1L << (caseEnPassant-7) & ~Bitboard.Afile)
+                            | (1L << (caseEnPassant-9) & ~Bitboard.Hfile) );
+    while (mangeurs != 0) {
+      int startSquare = Long.numberOfTrailingZeros(mangeurs);
+      moves.add(new Move(startSquare, caseEnPassant, MoveFlag.EnPassant));
+      mangeurs &= mangeurs - 1;
+    }
   }
 
   private void addPromotionMoves(ArrayList<Move> moves, int startSquare, int endSquare) {
